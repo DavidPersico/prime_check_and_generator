2f6b81fab25d7ef32d96a3c2070ff84168566390
DavidPersico <34723505+DavidPersico@users.noreply.github.com>
2020-04-16 13:56:35 -0600

    Create prime_check header

---
+++ prime_check.cpp
@@ -0,0 +1,9 @@
+#ifndef _PRIME_CHECK_
+#define _PRIME_CHECK_
+
+#include <bits/stdc++.h>
+
+/* Returns true when given positive prime number */
+bool prime_check(std::size_t n);
+
+#endif /* _PRIME_CHECK_ */
